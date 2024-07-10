39eedbe4eb11afa9143f5b8b0e10806641e20628
marti <martifabrega@gmail.com>
2024-07-10 15:58:41 +0200

    Add popup backbone to edit log

--- client/src/common/components/LogsContainer/LogsContainer.tsx
+++ client/src/common/components/LogsContainer/LogsContainer.tsx
@@ -1,5 +1,8 @@
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import Log from "./Log";
+import Icon from "@mui/material/Icon";
+import { EditRounded } from "@mui/icons-material";
+import { useState } from "react";
 
 interface Props {
     title: string
@@ -9,18 +12,31 @@ interface Props {
 const LogsContainer = ({ title }: Props) => {
     const dispatch = useAppDispatch();
     const logs = useAppSelector(state => state.logs);
+    const [editMode, setEditMode] = useState(false);
+
+    const handleEnableEditMode = () => {
+        setEditMode(!editMode);
+    }
 
     return (
         <div className="logs-wrapper">
             <h2>{title}</h2>
+            <Icon
+                component={EditRounded}
+                className={`edit-logs ${editMode ? 'selected' : ''}`}
+                onClick={handleEnableEditMode}    
+            />
             <div className="logs">
                 {logs.logs.map(log => 
                     <Log
                         key={log.id}
+                        id={log.id}
                         emoji={log.emoji}
                         label ={log.label}
                         datetime={log.datetime}
-                        text={log.text}/>
+                        text={log.text}
+                        editMode={editMode}
+                    />
                 )}
             </div>
         </div>
